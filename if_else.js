888ffa83224daa5239812b5b3eeeee6b5b26ab5c
K-Siim <kevinsiim007@gmail.com>
2024-11-20 13:56:42 +0200

    sda

---
+++ if_else.js
@@ -0,0 +1,22 @@
+/**
+ * IF :: ELSE
+ */
+
+const numberOfLegs = 4;
+const numberOfLegsiHave = 2;
+
+if (numberOfLegsiHave < numberOfLegs) {
+    console.log("I am human or humanoid")
+} else if (numberOfLegsiHave === numberOfLegs) {
+    console.log("I am a dog");
+} else {
+    console.log("Im a spider.");
+}
+
+//if() {
+    // if true, this code block will execute
+//} else if () {
+    // if true, this will execute
+//} else {
+    //if nothing above was true, i will execute
+//}
