dbd61e09ea3b64cc6c3eb93c8c53876c1bad953d
K-Siim <kevinsiim007@gmail.com>
2024-12-05 11:45:45 +0200

    fififx

--- dom.js
+++ dom.js
@@ -1,34 +1,76 @@
-/**
- * DOM API
- * Kuidas kasutada Javascripti HTMLi manipuleerimiseks
- * Saab kasutada HTML dokumente, kuhu on seatud see js fail.
- */
-
-const h1Element = document.querySelector('h1');
-h1Element.innerText = "Head teed!"
+const h1element = document.querySelector('h1');
+h1element .innerText = "Head Teed!"
 
 
 const fruits = ["Apple", "Orange", "Banana", "Mango", "Kiwi"];
 
-const fruitListContainer = document.querySelector('#fruit-list');
-const orderedListElement = document.createElement('ol');
+const fruitListCointainer = document.querySelector('#fruit-list');
 
-for(let i = 0; i < fruits.length; i++) {
-    const listElement = document.createElement('li');
-    listElement.innerText = fruits[i];
-    orderedListElement.appendChild(listElement);
-}
 
-fruitListContainer.appendChild(orderedListElement);
+function renderList() {
+    const orderedListElement = document.createElement('ol');
+    fruitListCointainer.innerHTML = "";
+    
+    for(let i = 0; i < fruits.length; i++) {
+        const listElement = document.createElement('li');
+
+        const spanElement = document.createElement('span');
+        listElement.innerText = fruits[i];
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = "🗑️";
+        deleteButton.classList.add('delete-btn');
+        deleteButton.setAttribute('data-index', i); 
+
+
+        listElement.appendChild(spanElement);
+        listElement.appendChild(deleteButton);
+
+        orderedListElement.appendChild(listElement);
+
+    }
+    fruitListCointainer.appendChild(orderedListElement);
+};
+
+renderList();
+
+
 
 
 const dontTouchMeButton = document.querySelector('#dont-touch-me-button');
 
 dontTouchMeButton.addEventListener('click', (event) => {
     console.log(event);
-    alert("Ei tohtinud ju.");
+    alert("Ei tohtinud ju");
 });
 
+const addNewFruitForm = document.querySelector('#add-new-fruits-form');
+addNewFruitForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const fruitInput = formData.get('fruit');
+    console.log(fruitInput);
+
+    if(!fruitInput.trim()) return;
+
+    fruits.push(fruitInput);
+    renderList();
+
+    event.target.reset();
+    
 
 
+});
 
+// Kustutame elemente
+
+fruitListCointainer.addEventListener('click', (event) => {
+    const buttonElement = event.target.closest('.delete-btn');
+    const index = parseInt(buttonElement.getAttribute("data-index"));
+
+    // TODO imploment check for index
+        fruits.splice(index, 1);
+         renderList()   
+   
+});
